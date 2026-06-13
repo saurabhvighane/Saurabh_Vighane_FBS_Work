212922e9aaf030645ed01243fdbd1e2eedf4e5e8
Saurabh Vighane <142729491+saurabhvighane@users.noreply.github.com>
2026-06-13 12:40:16 +0530

    Delete Core Python/Assignments/Assignment_2 directory

--- Core Python/Assignments/Assignment_2/Q5.py
+++
@@ -1,9 +0,0 @@
-# WAP to calculate selling price of book based on cost price and discount.
-
-cost=(int(input('enter cost price of book')))
-discount=int(input('enter discount(%)'))
-
-disc=(cost*discount)/100 
-sp=cost-disc
-
-print('selling price is:',sp)
